b6bced86392a87338745c5a7a364ad019a73cf19
TiepPt <tieppt07@gmail.com>
2020-06-18 09:59:53 +0700

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,74 +14,199 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          alignment: Alignment(0.0, 0.0),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.orange, Colors.red]
-            )
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/connexion.png',
-                height: 150,
-                fit: BoxFit.fill,
+    return new Scaffold(
+      body: new Center(
+        child: new ConstrainedBox(
+          constraints: new BoxConstraints.expand(),
+          child: new DecoratedBox(
+            decoration: new BoxDecoration(
+              gradient: new LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[Colors.deepOrangeAccent, Colors.deepOrangeAccent, Colors.pinkAccent, Colors.pink]
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'CONN',
-                    style: TextStyle(color: Colors.blue),
+            ),
+            child: Center(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment:  CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container (
+                        child: SizedBox(
+                          height: 155.0,
+                          child: Image.asset(
+                            'assets/images/connexion.png',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                      Container (
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('CONN1',
+                                style: Theme.of(context).textTheme.headline4.copyWith(
+                                    color: Colors.pink,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
+
+                              Text('EXION',
+                                style: Theme.of(context).textTheme.headline4.copyWith(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ],
+                          )
+                      ),
+                      Container (
+                        width: 200,
+                        child: Padding (
+                          padding: EdgeInsets.fromLTRB(0, 0, 0, 60),
+                          child: Text('Find and Meet people around you to find LOVE',
+                            style: Theme.of(context).textTheme.headline6.copyWith(
+                              color: Colors.white,
+                              fontSize: 15,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Container (
+                        width: 500,
+                        height: 100,
+                        child: Padding (
+                          padding: const EdgeInsets.all(15.0),
+                          child: RaisedButton (
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(35.0),
+                              side: BorderSide(color: Colors.red),
+                            ),
+                            onPressed: () {},
+                            color: Colors.white,
+                            textColor: Colors.red,
+                            child: Container (
+                              child: Row (
+                                children: <Widget>[
+                                  Image.asset(
+                                    'assets/images/ic_fb.png',
+                                    width: 40,
+                                    height: 40,
+                                    fit: BoxFit.fill,
+                                  ),
+                                  VerticalDivider (
+                                    color: Colors.pink,
+                                    thickness: 1,
+                                    indent: 18,
+                                    endIndent: 18,
+                                  ),
+                                  Text('Sign in with Facebook',
+                                    style: Theme.of(context).textTheme.headline4.copyWith(
+                                        color: Colors.pink,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w400
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container (
+                        width: 500,
+                        height: 100,
+                        child: Padding (
+                          padding: const EdgeInsets.all(15.0),
+                          child: RaisedButton (
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(35.0),
+                              side: BorderSide(color: Colors.red),
+                            ),
+                            onPressed: () {},
+                            color: Colors.white,
+                            textColor: Colors.red,
+                            child: Container (
+                              child: Row (
+                                children: [
+                                  Image.asset(
+                                    'assets/images/ic_tw.png',
+                                    width: 40,
+                                    height: 40,
+                                    fit: BoxFit.fill,
+                                  ),
+                                  VerticalDivider (
+                                    color: Colors.pink,
+                                    thickness: 1,
+                                    indent: 18,
+                                    endIndent: 18,
+                                  ),
+                                  Text('Sign in with Twitter',
+                                    style: Theme.of(context).textTheme.headline4.copyWith(
+                                        color: Colors.pink,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w400
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container (
+                        width: 500,
+                        height: 100,
+                        child: Padding (
+                          padding: const EdgeInsets.all(15.0),
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(35.0),
+                              side: BorderSide(color: Colors.red),
+                            ),
+                            onPressed: () {},
+                            color: Colors.white,
+                            textColor: Colors.red,
+                            child: Text('Sign Up',
+                                style: Theme.of(context).textTheme.headline4.copyWith(
+                                    fontSize: 20,
+                                    color: Colors.pink,
+                                    fontWeight: FontWeight.w400
+                                )
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container (
+                        child: Padding (
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text('ALREADY REGISTERED? SIGN IN',
+                            style: Theme.of(context).textTheme.headline4.copyWith(
+                                color: Colors.white,
+                                fontSize: 15,
+                                decoration: TextDecoration.underline,
+                                fontWeight: FontWeight.w500
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    'EXION',
-                    style: TextStyle(color: Colors.amberAccent),
-                  ),
-                ],
+                ),
               ),
-              Column(
-                children: [
-                  Text(
-                    'Find and meet people around',
-                    style: TextStyle(color: Colors.amberAccent),
-                  ),
-                  Texxt(
-                    ''
-                  )
-                ]
-              )
-            ],
+            ),
           ),
         ),
       ),
